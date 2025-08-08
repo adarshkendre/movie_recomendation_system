014867769b986cd55f22ca04efa6c940049cf28f
adarshkendre <adarshak16047@gmail.com>
2025-08-08 21:23:13 +0530

    enhanced recommender system with improved UI and performance

--- recommender.py
+++ recommender.py
@@ -44,8 +44,9 @@ movies['overview'] = movies['overview'].apply(lambda x: x.split())
 movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ", "") for i in x])
 movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
 
-new_df = movies[['movie_id', 'title', 'tags']]
-new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x) if isinstance(x, (list, tuple)) else "")
+# Fix for pandas warnings - create a proper copy of the DataFrame
+new_df = movies[['movie_id', 'title', 'tags']].copy()
+new_df.loc[:, 'tags'] = new_df['tags'].apply(lambda x: " ".join(x) if isinstance(x, (list, tuple)) else "")
 
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
@@ -56,15 +57,20 @@ def stem(text):
         y.append(ps.stem(i))
     return " ".join(y)
 
-new_df['tags'] = new_df['tags'].apply(stem)
-new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
+# Fix for pandas warnings - use .loc for assignment
+new_df.loc[:, 'tags'] = new_df['tags'].apply(stem)
+new_df.loc[:, 'tags'] = new_df['tags'].apply(lambda x: x.lower())
 
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vectors = cv.fit_transform(new_df['tags']).toarray()
 similarity = cosine_similarity(vectors)
 
 def recommend(movie):
-    movie_index = new_df[new_df['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    return [new_df.iloc[i[0]].title for i in movies_list]
+    try:
+        movie_index = new_df[new_df['title'] == movie].index[0]
+        distances = similarity[movie_index]
+        movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+        return [new_df.iloc[i[0]].title for i in movies_list]
+    except IndexError:
+        # Handle case where movie is not found
+        return []
